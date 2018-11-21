602154b43ff467186022eba39753986777f450b0
SuperJedrzej <j.kubiak164@gmail.com>
2018-11-21 22:45:36 +0100

    part5

--- GigHub/Models/Notification.cs
+++ GigHub/Models/Notification.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Web.UI;
 
 namespace GigHub.Models
 {
@@ -8,8 +9,8 @@ namespace GigHub.Models
         public int Id { get; set; }
         public DateTime DateTime { get; private set; }
         public NotificationType Type { get; private set; }
-        public DateTime? OriginalDateTime { get; set; }
-        public string OriginalVenue { get; set; }
+        public DateTime? OriginalDateTime { get; private set; }
+        public string OriginalVenue { get; private set; }
 
         [Required]
         public Gig Gig { get; private set; }
@@ -26,7 +27,24 @@ namespace GigHub.Models
             Type = type;
             Gig = gig;
             DateTime = DateTime.Now;
+        }
 
+        public static Notification GigCreated(Gig gig)
+        {
+            return  new Notification(NotificationType.GigCreated,gig);
+        }
+        public static Notification GigUpdated(Gig newgig,DateTime originalDateTime,string originalVenue)
+        {
+            var notification =   new Notification(NotificationType.GigUpdated,newgig);
+            notification.OriginalDateTime = originalDateTime;
+            notification.OriginalVenue = originalVenue;
+
+            return notification;
+        }
+
+        public static Notification GigCanceled(Gig gig)
+        {
+            return new Notification(NotificationType.GigCanceled,gig);
         }
     }
 }
